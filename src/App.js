dd6bad7e379dee51043313fe18fac4c926e77757
Aybis <muchtara2m@gmail.com>
2021-08-09 13:10:19 +0700

    add component item for arrived

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from './components/Header.js';
 import Hero from './components/Hero';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Browse from './components/Browse.js';
 import Arrived from './components/Arrived.js';
 import AsideMenu from './components/AsideMenu';
@@ -8,12 +8,31 @@ import Clients from './components/Clients.js';
 import Footer from 'components/Footer.js';
 
 function App() {
+  const [items, setitems] = useState([]);
+
+  useEffect(function () {
+    (async function () {
+      const response = await fetch(
+        'https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc',
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            accept: 'application/json',
+            'x-api-key': process.env.REACT_APP_API_KEY,
+          },
+        },
+      );
+      const { nodes } = await response.json();
+      setitems(nodes);
+    })();
+  }, []);
+
   return (
     <>
       <Header />
       <Hero />
       <Browse />
-      <Arrived />
+      <Arrived items={items} />
       <Clients />
       <AsideMenu />
       <Footer />
