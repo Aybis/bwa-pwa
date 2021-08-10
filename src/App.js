9b7613d673b03afdab309225003e253433acaaee
Aybis <muchtara2m@gmail.com>
2021-08-10 13:11:20 +0700

    info offline add icon for install

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ import Arrived from './components/Arrived.js';
 import AsideMenu from './components/AsideMenu';
 import Clients from './components/Clients.js';
 import Footer from 'components/Footer.js';
+import Offline from './components/Offline.js';
 
 function App() {
   const [items, setitems] = useState([]);
+  const [isOffline, setisOffline] = useState(!navigator.onLine);
+
+  const handleOfflineStatus = () => {
+    setisOffline(!navigator.onLine);
+  };
 
   const getDataApi = async () => {
     await fetch('https://batikita.herokuapp.com/index.php/batik/all')
@@ -16,23 +22,31 @@ function App() {
       .then((response) => {
         setitems(response.hasil);
         const script = document.createElement('script');
-        script.src = './js/carousel.js';
+        script.src = './carousel.js';
         script.async = false;
         document.body.appendChild(script);
       });
   };
+
   useEffect(() => {
+    handleOfflineStatus();
+    window.addEventListener('online', handleOfflineStatus);
+    window.addEventListener('offline', handleOfflineStatus);
     const timeOut = setTimeout(() => {
       getDataApi();
     }, 300);
     return () => {
+      window.removeEventListener('online', handleOfflineStatus);
+      window.removeEventListener('offline', handleOfflineStatus);
+
       clearTimeout(timeOut);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [isOffline]);
 
   return (
     <>
+      {isOffline && <Offline />}
       <Header />
       <Hero />
       <Browse />
